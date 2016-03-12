2b3294e8703d297564e8971f01502bf5dcd18a0a
guillaume-boutin <guillaumeboutin2101@gmail.com>
2016-03-11 23:29:26 -0500

    Finished AngularJS Tutorial 28

--- controller.js
+++ controller.js
@@ -1,33 +1,8 @@
 var app = angular.module('mainApp', []);
 
-app.service('fromService', function() {
-    this.message = "This is from service";
-});
-
-app.factory('fromFactory', function() {
-    return {
-        message: "This is from factory",
-    }
-});
-
-app.provider('fromProvider', function() {
-    var m1 = "This is from provider";
-    return {
-        $get: function() {
-            return {
-                message: m1,
-            };
-        },
-        setAName: function(name) {
-            m1 += ' ' + name;
-        }
+app.controller('app', function($scope) {
+    $scope.show = 'msg1';
+    $scope.switchMsg = function() {
+        $scope.show = $scope.show === 'msg1' ? 'msg2' : 'msg1';
     };
-});
-
-app.config(function(fromProviderProvider) {
-    fromProviderProvider.setAName('Guillaume');
-});
-
-app.controller('app', function($scope, fromService, fromFactory, fromProvider) {
-    $scope.greetMessages = [fromService.message, fromFactory.message, fromProvider.message];
 });
