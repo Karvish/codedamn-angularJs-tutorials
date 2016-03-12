efbc51f3fbefe09463b42e4a4eba5bbacf08222e
guillaume-boutin <guillaumeboutin2101@gmail.com>
2016-03-11 19:05:38 -0500

    Finished AngularJS Tutorial 20

--- controller.js
+++ controller.js
@@ -1,8 +1,6 @@
 var app = angular.module('mainApp', []);
 
-app.controller('people', function($scope, $http) {
-    $http.get('/database.JSON')
-    .success(function(response) {
-        $scope.people = response.records;
-    });
+app.controller('app', function($scope) {
+    var range = new Array(25);
+    $scope.myRange = range;
 });
