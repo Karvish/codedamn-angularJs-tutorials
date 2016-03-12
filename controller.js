23785ecf78a523902dc94eea76ffbe2a8589786c
guillaume-boutin <guillaumeboutin2101@gmail.com>
2016-03-11 20:17:52 -0500

    Finished AngularJS Tutorial 25

--- controller.js
+++ controller.js
@@ -1,28 +1,38 @@
 var app = angular.module('mainApp', []);
 
-app.controller('app', function($scope, randomService, randomFactory) {
+app.controller('app', function($scope, date) {
 
-    $scope.generateRandom = function() {
-        $scope.serviceNum = randomService.generate();
-        $scope.factoryNum = randomFactory.generate();
-    }
+    $scope.message = date.showDate();
 
 });
 
-app.service('randomService', function() {
-
-    this.generate = function() {
-        return Math.floor(Math.random()*10);
-    }
-
-});
-
-app.factory('randomFactory', function() {
-
+app.provider('date', function() {
     return {
-        generate: function() {
-            return Math.floor(Math.random()*10);
-        },
-    };
-
+        $get: function() {
+            return {
+                showDate: function() {
+                    var date = new Date();
+                    return date.getHours();
+                }
+            }
+        }
+    }
 });
+
+// app.service('randomService', function() {
+//
+//     this.generate = function() {
+//         return Math.floor(Math.random()*10);
+//     }
+//
+// });
+//
+// app.factory('randomFactory', function() {
+//
+//     return {
+//         generate: function() {
+//             return Math.floor(Math.random()*10);
+//         },
+//     };
+//
+// });
