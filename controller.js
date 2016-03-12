a653f933055583d5ddebee9ce9c8cb3ac1853041
guillaume-boutin <guillaumeboutin2101@gmail.com>
2016-03-11 22:58:37 -0500

    Finished AngularJS Tutorial 26

--- controller.js
+++ controller.js
@@ -7,15 +7,38 @@ app.controller('app', function($scope, date) {
 });
 
 app.provider('date', function() {
+    var greet;
+    var date = new Date();
     return {
+        setGreeting: function(value) {
+            greet = value;
+        },
+        getTime: function() {
+            return date.getHours();
+        },
         $get: function() {
             return {
                 showDate: function() {
-                    var date = new Date();
-                    return date.getHours();
+                    return greet + " It's " + date.getHours();
                 }
             }
         }
+    }
+});
+
+app.config(function(dateProvider) {
+    var hour = dateProvider.getTime();
+    if(hour > 23 && hour <= 11) {
+        dateProvider.setGreeting("Good morning!");
+    }
+    else if (hour > 11 && hour <= 16) {
+        dateProvider.setGreeting("Good afternoon!");
+    }
+    else if (hour > 16 && hour <= 20){
+        dateProvider.setGreeting("Good evening!");
+    }
+    else {
+        dateProvider.setGreeting("Good night!");
     }
 });
 
